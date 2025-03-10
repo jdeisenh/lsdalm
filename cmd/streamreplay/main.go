c413196197f3a8c6e1413d85415de3ab28de670d
Jo Deisenhofer <jo.deisenhofer@gmail.com>
2025-03-10 18:29:14 +0100

    new cmd streamreplay to just replay old recordings, no loop

---
+++ cmd/streamreplay/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"os"
+	"time"
+
+	streamgetter "github.com/jdeisenh/lsdalm/pkg/lsdalm"
+	"github.com/rs/zerolog"
+)
+
+func main() {
+
+	logger := zerolog.New(zerolog.ConsoleWriter{
+		Out:        os.Stderr,
+		TimeFormat: time.TimeOnly,
+	}).With().Timestamp().Logger()
+
+	debug := flag.Bool("debug", false, "set log level to debug")
+	dump := flag.String("dumpdir", "", "Directory to dump segments")
+	listen := flag.String("listen", ":9080", "Adress/port to listen")
+
+	flag.Parse()
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	if *debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	}
+	if *dump == "" {
+		flag.Usage()
+		return
+	}
+	var err error
+	sg, err := streamgetter.NewStreamReplay(*dump, logger)
+	if err != nil {
+		logger.Fatal().Err(err).Send()
+		return
+	}
+	// Todo: Load data
+	// Paths for segments
+	http.HandleFunc("/manifest.mpd", sg.Handler)
+	http.HandleFunc("/", sg.FileHandler)
+	logger.Fatal().Err(http.ListenAndServe(*listen, nil)).Send()
+}
---
+++ pkg/lsdalm/streamreplay.go
@@ -0,0 +1,318 @@
+package streamgetter
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"strconv"
+	"time"
+
+	"github.com/jdeisenh/lsdalm/pkg/go-mpd"
+	"github.com/rs/zerolog"
+)
+
+type StreamReplay struct {
+	dumpdir     string
+	manifestDir string
+
+	logger                   zerolog.Logger
+	history                  []HistoryElement
+	historyStart, historyEnd time.Time
+}
+
+func NewStreamReplay(dumpdir string, logger zerolog.Logger) (*StreamReplay, error) {
+
+	st := &StreamReplay{
+		dumpdir:     dumpdir,
+		manifestDir: path.Join(dumpdir, ManifestPath),
+		logger:      logger,
+		history:     make([]HistoryElement, 0, 1000),
+	}
+	st.fillData()
+	if len(st.history) < 10 {
+		return nil, fmt.Errorf("Not enough manifests")
+	}
+	st.ShowStats()
+	return st, nil
+}
+
+// fillData reads add the manifests
+func (sc *StreamReplay) fillData() error {
+	files, err := os.ReadDir(sc.manifestDir)
+	if err != nil {
+		sc.logger.Error().Err(err).Msg("Scan directories")
+		return err
+	}
+	var lasttime time.Time
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		sc.logger.Trace().Msg(f.Name())
+		ctime, err := time.Parse(ManifestFormat, f.Name())
+		if err != nil {
+			sc.logger.Warn().Err(err).Msg("Parse String")
+			continue
+		}
+		if !lasttime.IsZero() && (ctime.Sub(lasttime) > time.Second*30) {
+			sc.logger.Error().Msgf("Too large a gap between %s and %s, dropping",
+				lasttime.Format(time.TimeOnly), ctime.Format(time.TimeOnly))
+			sc.history = sc.history[:0]
+		}
+
+		sc.history = append(sc.history, HistoryElement{At: ctime, Filename: f.Name()})
+	}
+	// Find last pts in both first and last manifest
+	fs := sc.history[0].At
+	first, err := sc.loadHistoricMpd(fs)
+	if err != nil {
+		sc.logger.Warn().Err(err).Msg("Cannot load")
+	}
+	ff, fl, _ := sc.getPtsRange(first, "video/mp4")
+
+	ls := sc.history[len(sc.history)-1].At
+	last, err := sc.loadHistoricMpd(ls)
+	if err != nil {
+		sc.logger.Warn().Err(err).Msg("Cannot load")
+	}
+	lf, ll, _ := sc.getPtsRange(last, "video/mp4")
+
+	sc.logger.Debug().Msgf("Start %s %s-%s", shortT(fs), Round(fs.Sub(ff)), Round(fs.Sub(fl)))
+	sc.logger.Debug().Msgf("End %s %s-%s", shortT(ls), Round(ls.Sub(lf)), Round(ls.Sub(ll)))
+	sc.historyStart = fl
+	sc.historyEnd = ll
+
+	return nil
+}
+
+// FindHistory returns the newest element from history older than 'want'
+func (sc *StreamReplay) FindHistory(want time.Time) *HistoryElement {
+
+	ret := findSub(sc.history, want)
+	if ret == nil {
+		return nil
+	}
+	acopy := *ret
+	acopy.Filename = path.Join(sc.manifestDir, ret.Filename)
+	return &acopy
+}
+
+func (sc *StreamReplay) getRecordingRange() (from, to time.Time) {
+	from = sc.history[0].At
+	to = sc.history[len(sc.history)-1].At
+
+	return
+}
+
+// Return loop metadata
+// for the position at, return offset (to recording), timeshift und loop duration
+func (sc *StreamReplay) getLoopMeta(at, now time.Time, requestDuration time.Duration) (offset, shift, duration time.Duration, start time.Time) {
+
+	// Calculate the offset in the recording buffer and the timeshift (added to timestamps)
+	// Invariants:
+	// start+offset+shift=now => shift=now-offset-start
+	// We play at at%duration => (start+offset)%duration == at%duration => offset=(at-start)%duration
+
+	// Data from history buffer
+	// This is inexact, the last might not have all Segments downloaded
+	start, _ = sc.getRecordingRange()
+	duration = sc.historyEnd.Sub(sc.historyStart)
+
+	offset = at.Sub(start) % duration
+	shift = now.Add(-offset).Sub(start)
+	//sc.logger.Info().Msgf("%s %s", at, start)
+	return
+}
+
+// AdjustMpd adds a time offset to each Period in the Manifest, shifting the PresentationTime
+func (sc *StreamReplay) AdjustMpd(mpde *mpd.MPD, shift time.Duration) {
+	if len(mpde.Period) == 0 {
+		return
+	}
+	for _, period := range mpde.Period {
+
+		// Shift periods
+		if period.Start != nil {
+			startmed, _ := (*period.Start).ToNanoseconds()
+			start := time.Duration(startmed)
+			*period.Start = DurationToXsdDuration(start + shift)
+		}
+	}
+	return
+}
+
+// Find a manifest at time 'at'
+func (sc *StreamReplay) loadHistoricMpd(at time.Time) (*mpd.MPD, error) {
+
+	sourceElement := sc.FindHistory(at)
+
+	if sourceElement == nil {
+		return nil, errors.New("No source found")
+	}
+	buf, err := os.ReadFile(sourceElement.Filename)
+	if err != nil {
+		return nil, err
+	}
+	mpde := new(mpd.MPD)
+	if err := mpde.Decode(buf); err != nil {
+		return nil, err
+	}
+	return mpde, nil
+}
+
+// GetLooped generates a Manifest by finding the manifest before now%duration
+func (sc *StreamReplay) GetLooped(at, now time.Time, requestDuration time.Duration) ([]byte, error) {
+
+	offset, shift, duration, startOfRecording := sc.getLoopMeta(at, now, requestDuration)
+	sc.logger.Info().Msgf("Offset: %s TimeShift: %s LoopDuration: %s LoopStart:%s Original At %s",
+		RoundToS(offset), RoundToS(shift), RoundToS(duration), shortT(startOfRecording), shortT(startOfRecording.Add(offset)))
+	mpdCurrent, err := sc.loadHistoricMpd(startOfRecording.Add(offset))
+	if err != nil {
+		return []byte{}, err
+	}
+
+	sc.AdjustMpd(mpdCurrent, shift) // Manipulate
+
+	//sc.logger.Info().Msgf("Move period: %s", startOfRecording.Add(shift))
+
+	// re-encode
+	afterEncode, err := mpdCurrent.Encode()
+	if err != nil {
+		return nil, err
+	}
+	return afterEncode, nil
+}
+
+// Iterate through all periods, representation, segmentTimeline and
+func (sc *StreamReplay) getPtsRange(mpde *mpd.MPD, mimetype string) (time.Time, time.Time, error) {
+
+	if len(mpde.Period) == 0 {
+		return time.Time{}, time.Time{}, errors.New("No periods")
+	}
+	var ast time.Time
+	if mpde.AvailabilityStartTime != nil {
+		ast = time.Time(*mpde.AvailabilityStartTime)
+	}
+
+	var earliest, latest time.Time
+
+	for _, period := range mpde.Period {
+		for _, as := range period.AdaptationSets {
+
+			// If there is no segmentTimeline, skip it
+			if as.SegmentTemplate == nil || as.SegmentTemplate.SegmentTimeline == nil || len(as.SegmentTemplate.SegmentTimeline.S) == 0 {
+				continue
+			}
+			if as.MimeType != mimetype {
+				continue
+			}
+
+			// Calculate period start
+			var start time.Duration
+			if period.Start != nil {
+				startmed, _ := (*period.Start).ToNanoseconds()
+				start = time.Duration(startmed)
+			}
+			periodStart := ast.Add(start)
+			from, to := sumSegmentTemplate(as.SegmentTemplate, periodStart)
+			if from.IsZero() {
+				for _, pres := range as.Representations {
+					if pres.SegmentTemplate != nil {
+						from, to = sumSegmentTemplate(pres.SegmentTemplate, periodStart)
+						break
+					}
+				}
+			}
+			if earliest.IsZero() || from.Before(earliest) {
+				earliest = from
+			}
+			if to.After(latest) {
+				latest = to
+			}
+		}
+	}
+	return earliest, latest, nil
+}
+
+// Handler serves manifests
+func (sc *StreamReplay) Handler(w http.ResponseWriter, r *http.Request) {
+
+	now := time.Now()
+	startat := now
+	var duration time.Duration
+
+	// Parse time from query Args
+
+	// to timeoffset
+	ts := r.URL.Query()["to"]
+	if len(ts) > 0 {
+		t, err := strconv.Atoi(ts[0])
+		if err != nil {
+			sc.logger.Warn().Err(err).Msg("Parse time")
+		} else if t < 0 && t > 1e6 {
+			sc.logger.Warn().Msg("Implausable time offset, ignoring")
+		} else {
+			startat = startat.Add(-time.Duration(t) * time.Second)
+		}
+	}
+
+	// ld loop duration
+	ld := r.URL.Query()["ld"]
+	if len(ld) > 0 {
+		t, err := strconv.Atoi(ld[0])
+		if err != nil {
+			sc.logger.Warn().Err(err).Msg("Parse time")
+		} else if t <= 0 && t > 1e5 {
+			sc.logger.Warn().Msg("Implausable duration, ignoring")
+		} else {
+			duration = time.Duration(t) * time.Second
+		}
+	}
+
+	buf, err := sc.GetLooped(startat, now, duration)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.Header().Add("Content-Type", "application/dash+xml")
+	w.Write(buf)
+}
+
+// FileHanlder serves data
+func (sc *StreamReplay) FileHandler(w http.ResponseWriter, r *http.Request) {
+	//urlpath := strings.TrimPrefix(r.URL.Path, "/dash/")
+	filepath := path.Join(sc.dumpdir, r.URL.Path)
+	sc.logger.Trace().Str("path", filepath).Msg("Access")
+	http.ServeFile(w, r, filepath)
+}
+
+func (sc *StreamReplay) ShowStats() {
+	if len(sc.history) > 0 {
+		first := sc.history[0].At
+		last := sc.history[len(sc.history)-1].At
+		sc.logger.Info().Msgf("Recorded %d manifests from %s to %s (%s)",
+			len(sc.history),
+			first.Format(time.TimeOnly),
+			last.Format(time.TimeOnly),
+			last.Sub(first),
+		)
+	}
+
+}
+
+// rewriteBaseUrl will return a URL concatenating upstream with base
+func (sc *StreamReplay) rewriteBaseUrl(base string, upstream *url.URL) string {
+	// Check for relative URL
+	ur, e := url.Parse(base)
+	if e != nil {
+		sc.logger.Warn().Err(e).Msg("URL parsing")
+		return base
+	}
+	if ur.IsAbs() {
+		return base
+	}
+	return upstream.ResolveReference(ur).String()
+}
